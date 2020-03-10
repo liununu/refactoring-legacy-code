763093605d3ba37ffef14b5942a8945db2c0c6af
liununu <liununu5@gmail.com>
2020-03-10 14:47:45 +0800

    test(legacy): add test for create wallet transaction instance without pre assigned id

--- src/test/java/cn/xpbootcamp/legacy_code/WalletTransactionTest.java
+++ src/test/java/cn/xpbootcamp/legacy_code/WalletTransactionTest.java
@@ -2,14 +2,21 @@ package cn.xpbootcamp.legacy_code;
 
 import cn.xpbootcamp.legacy_code.enums.STATUS;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.lang.reflect.Field;
 import java.util.UUID;
+import java.util.stream.Stream;
 
 import static cn.xpbootcamp.legacy_code.enums.STATUS.TO_BE_EXECUTED;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class WalletTransactionTest {
+
+    private static Stream<String> nullAndEmptyStrings() {
+        return Stream.of("", null);
+    }
 
     @Test
     void should_return_wallet_transaction_when_create_success()
@@ -34,6 +41,21 @@ class WalletTransactionTest {
         assertThat(getPrivateField(walletTransaction, "status", STATUS.class))
                 .isEqualTo(TO_BE_EXECUTED);
         assertThat(getPrivateField(walletTransaction, "walletTransactionId", String.class)).isNull();
+    }
+
+    @ParameterizedTest
+    @MethodSource("nullAndEmptyStrings")
+    void should_return_wallet_transaction_with_id_when_create_given_without_pre_assigned_id(String preAssignedId)
+            throws NoSuchFieldException, IllegalAccessException {
+        Long buyerId = 123L;
+        Long sellerId = 234L;
+        Long productId = 8989L;
+        String orderId = UUID.randomUUID().toString();
+
+        WalletTransaction walletTransaction =
+                new WalletTransaction(preAssignedId, buyerId, sellerId, productId, orderId);
+
+        assertThat(getPrivateField(walletTransaction, "id", String.class)).startsWith("t_");
     }
 
     private <T> T getPrivateField(
